eef952e9c222397bcfa660dc6bae97f12a216a32
Dan Hook <s188873@uos.ac.uk>
2025-01-24 21:25:08 +0000

    Added score manager script

---
+++ Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private int scorePerSecond = 2;
+    
+    private int _score;
+    private float _scoreDelay;
+    private float _nextScoreTime; 
+
+    private void Awake()
+    {
+        _scoreDelay = 1f / scorePerSecond;
+        _nextScoreTime = Time.time + _scoreDelay;
+    }
+
+    // There's probably a better way to do this for increasing the score. I did try a coroutine, but it had a weird
+    // hitch/delay every second score increase, which made it look weird. This will do for now. 
+    private void Update()
+    {
+        if (_nextScoreTime > Time.time) return;
+        
+        _score++; 
+        Debug.Log("Score: " + _score);
+        _nextScoreTime = Time.time + _scoreDelay;
+    }
+}
