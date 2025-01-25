e2df944e238e6f087152372f41a594f500b96eef
Dan Hook <s188873@uos.ac.uk>
2025-01-25 15:21:50 +0000

    Update ObjectSpawner.cs

--- Assets/_Scripts/ObjectSpawner.cs
+++ Assets/_Scripts/ObjectSpawner.cs
@@ -1,10 +1,32 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] obstacles;
     [SerializeField] private GameObject[] buffs;
     [SerializeField] private GameObject[] debuffs;
+
+    private Camera _camera; 
+    
+    private float _xLeftMax; 
+    private float _xRightMax;
+
+    private float _screenWidth; 
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Start()
+    {
+        _xLeftMax = CameraUtil.GetScreenLeftX(_camera);
+        _xRightMax = CameraUtil.GetScreenRightX(_camera);
+        
+        _screenWidth = _xRightMax - _xLeftMax;
+    }
 
     private void Update()
     {
@@ -21,7 +43,9 @@ public class ObjectSpawner : MonoBehaviour
         for (int i = 0; i < randomSpawnNumber; i++)
         {
             int randomIndex = Random.Range(0, obstacles.Length); // Which object from the array spawns
+            
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-2, 3), 5, Random.Range(-2, 3));
+            
             Instantiate(obstacles[randomIndex], randomSpawnPosition, obstacles[randomIndex].transform.rotation);
         }
     }
