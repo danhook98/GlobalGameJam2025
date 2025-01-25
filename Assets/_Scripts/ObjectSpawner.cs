b78fd16f3d022d93bdbb7639396b593a49756dd2
Nikolaos Comandariu <s283743@uos.ac.uk>
2025-01-25 11:54:39 +0000

    ObjectSpawner update

--- Assets/_Scripts/ObjectSpawner.cs
+++ Assets/_Scripts/ObjectSpawner.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] _obstacles;
-    //private int minSpawn = 1;
-    //private int maxSpawn = 3;
 
     void Update()
     {
@@ -16,12 +14,13 @@ public class ObjectSpawner : MonoBehaviour
     
     public void SpawnObstacle()
     {
-        int randomIndex = Random.Range(0, _obstacles.Length);
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-2, 3), 5, Random.Range(-2, 3));
-        int randomSpawnNumber = Random.Range(1, 4);
-
+        
+        int randomSpawnNumber = Random.Range(1, 4); // How many obstacles should spawn
+        
         for (int i = 0; i < randomSpawnNumber; i++)
         {
+            int randomIndex = Random.Range(0, _obstacles.Length); // Which object from the array spawns
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-2, 3), 5, Random.Range(-2, 3));
             Instantiate(_obstacles[randomIndex], randomSpawnPosition, Quaternion.identity);
         }
     }
