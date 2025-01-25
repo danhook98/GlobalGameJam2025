dd5b0d4d1110b60a9d262cf5c21ef91f12d4035c
Nikolaos Comandariu <s283743@uos.ac.uk>
2025-01-25 09:33:44 +0000

    Changed SceneStorageSO so it saves strings

--- Assets/_Scripts/SceneStorageSO.cs
+++ Assets/_Scripts/SceneStorageSO.cs
@@ -5,5 +5,5 @@ using UnityEngine.SceneManagement;
 [CreateAssetMenu(fileName = "SceneStorage", menuName = "BubbleGame/Scene Storage")]
 public class SceneStorageSO : ScriptableObject
 {
-    public Scene scene;
+    public string scene;
 }
