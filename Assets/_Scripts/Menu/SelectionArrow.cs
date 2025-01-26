ca535db26ccfb9619ba944c23a9da1e02bad52ee
Nikolaos Comandariu <s283743@uos.ac.uk>
2025-01-26 12:44:02 +0000

    Selection Arrow progress

--- Assets/_Scripts/Menu/SelectionArrow.cs
+++ Assets/_Scripts/Menu/SelectionArrow.cs
@@ -6,7 +6,8 @@ public class SelectionArrow : MonoBehaviour
 {
     [SerializeField] private RectTransform[] options;
     private RectTransform _rect;
-    private int currentPosition;
+    private int _currentPosition;
+    private int _previousPosition;
 
     private void Awake()
     {
@@ -28,20 +29,22 @@ public class SelectionArrow : MonoBehaviour
 
     private void ChangePosition(int change)
     {
-        currentPosition += change;
+        _currentPosition += change;
 
-        if (currentPosition < 0)
-            currentPosition = options.Length - 1;
-        else if (currentPosition > options.Length - 1)
-            currentPosition = 0;
+        if (_currentPosition < 0)
+            _currentPosition = options.Length - 1;
+        else if (_currentPosition > options.Length - 1)
+            _currentPosition = 0;
         
         //Assign the Y position of the current option to the arrow 
-        _rect.position = new Vector3(_rect.position.x, options[currentPosition].position.y, 0);
+        _rect.position = new Vector3(_rect.position.x, options[_currentPosition].position.y, 0);
+        options[_currentPosition].GetComponent<Image>().sprite = options[_currentPosition].GetComponent<Button>().spriteState.highlightedSprite;
+        options[_previousPosition].GetComponent<Image>().sprite = options[_currentPosition].GetComponent<Button>().spriteState.disabledSprite;
     }
 
     private void Interact()
     {
         // Access the button component on each option and call its function
-        options[currentPosition].GetComponent<Button>().onClick.Invoke();
+        options[_currentPosition].GetComponent<Button>().onClick.Invoke();
     }
 }
