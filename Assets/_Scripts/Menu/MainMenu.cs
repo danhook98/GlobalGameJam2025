aacd047904440a78ca9403a3e25d040e6f1920af
Linkgiggy2002 <connordennis19@googlemail.com>
2025-01-26 13:10:15 +0000

    PauseMenuWorking

--- Assets/_Scripts/Menu/MainMenu.cs
+++ Assets/_Scripts/Menu/MainMenu.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+    bool pause = false;
+    bool mainmenu = true;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && pause == false && mainmenu == false)
+        {
+            pause = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+       
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            mainmenu = false;
+        }
+    }
     public void PlayGame()
     {
+        mainmenu = false;
         SceneManager.LoadSceneAsync(1);
     }
 
@@ -15,4 +36,18 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void Home()
+    {
+        pause = false;
+        mainmenu = true;
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    { 
+        pause = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0;
+    }
 }
