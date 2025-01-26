08e3460e26c85ae65bfa74d12bf9374784d09df3
Nikolaos Comandariu <s283743@uos.ac.uk>
2025-01-26 15:32:23 +0000

    Audio settings fix

--- Assets/_Scripts/Audio/AudioSettings.cs
+++ Assets/_Scripts/Audio/AudioSettings.cs
@@ -32,7 +32,7 @@ public class AudioSettings : MonoBehaviour
     public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
-        myMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
