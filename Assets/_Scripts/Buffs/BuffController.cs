20c96729c90e97ef4df460d2d0964e3ae45c4c3c
Linkgiggy2002 <connordennis19@googlemail.com>
2025-01-26 16:32:40 +0000

    debuff/buff SFX code

--- Assets/_Scripts/Buffs/BuffController.cs
+++ Assets/_Scripts/Buffs/BuffController.cs
@@ -5,12 +5,15 @@ public class BuffController : ObstacleEntity
 {
     [SerializeField] private GameEventChannelSO gameEventChannel;
     [SerializeField] private BuffTypes buffType;
+    [SerializeField] private AudioEventChannelSO AudioEventChannel;
+    [SerializeField] private AudioClipSO buffcollectionclip;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return; 
         
         Debug.Log("Triggered buff collect");
+        AudioEventChannel.PlayAudio(buffcollectionclip);
         gameEventChannel.BuffCollected(buffType);
     }
 }
