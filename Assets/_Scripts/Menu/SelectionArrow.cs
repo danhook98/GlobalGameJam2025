80502d2865bb75a50712ac3851015cf0fd697ac5
Nikolaos Comandariu <s283743@uos.ac.uk>
2025-01-26 14:15:19 +0000

    Main Menu changes

--- Assets/_Scripts/Menu/SelectionArrow.cs
+++ Assets/_Scripts/Menu/SelectionArrow.cs
@@ -1,12 +1,13 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectionArrow : MonoBehaviour
 {
-    [SerializeField] private RectTransform[] options;
-    private RectTransform _rect;
-    private int _currentPosition;
+    [SerializeField] public RectTransform[] options;
+    public RectTransform _rect;
+    public int _currentPosition;
     private int _previousPosition;
 
     private void Awake()
@@ -30,21 +31,29 @@ public class SelectionArrow : MonoBehaviour
     private void ChangePosition(int change)
     {
         _currentPosition += change;
+        _previousPosition = _currentPosition - change;
 
         if (_currentPosition < 0)
             _currentPosition = options.Length - 1;
         else if (_currentPosition > options.Length - 1)
             _currentPosition = 0;
         
+        if (_previousPosition < 0)
+            _previousPosition = options.Length - 1;
+        else if (_previousPosition > options.Length - 1)
+            _previousPosition = 0;
+        
         //Assign the Y position of the current option to the arrow 
         _rect.position = new Vector3(_rect.position.x, options[_currentPosition].position.y, 0);
         options[_currentPosition].GetComponent<Image>().sprite = options[_currentPosition].GetComponent<Button>().spriteState.highlightedSprite;
-        options[_previousPosition].GetComponent<Image>().sprite = options[_currentPosition].GetComponent<Button>().spriteState.disabledSprite;
+        options[_previousPosition].GetComponent<Image>().sprite = options[_previousPosition].GetComponent<Button>().spriteState.disabledSprite;
     }
 
     private void Interact()
     {
         // Access the button component on each option and call its function
+        options[_currentPosition].GetComponent<Button>().Select();
         options[_currentPosition].GetComponent<Button>().onClick.Invoke();
+        
     }
 }
